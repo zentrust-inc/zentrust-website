919c1e5aeb9b6133c6707ace4faf1e4902c9e6ce
Zenslim <zenslim2010@gmail.com>
2025-12-09 15:14:58 +0545

    Update page.tsx

--- app/donate/payment/page.tsx
+++ app/donate/payment/page.tsx
@@ -1,7 +1,8 @@
+"use client"
+
 export const dynamic = "force-dynamic"
 export const fetchCache = "force-no-store"
 export const revalidate = 0
-"use client"
 
 import { useSearchParams } from "next/navigation"
 import { useEffect, useState } from "react"
