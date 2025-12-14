dcc90a4342c7d5b5c6b92338d59a516563b5417c
Zenslim <zenslim2010@gmail.com>
2025-12-14 16:36:41 +0545

    Update route.ts

--- app/api/stewardship/create-intent/route.ts
+++ app/api/stewardship/create-intent/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-04-10",
+  typescript: true,
 });
 
 // -----------------------------------------------------------------------------
