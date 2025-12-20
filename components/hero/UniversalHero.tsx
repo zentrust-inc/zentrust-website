76ec74641d9c9b7133e5810954f2d9f5ef587089
Zenslim <zenslim2010@gmail.com>
2025-12-20 18:14:34 +0545

    Update UniversalHero.tsx

--- components/hero/UniversalHero.tsx
+++ components/hero/UniversalHero.tsx
@@ -1,16 +1,8 @@
-'use client';
+"use client";
 
 import { HeroShell } from "./HeroShell";
 import { defaultHero, type HeroDefinition } from "./createHero";
 
-type UniversalHeroProps = {
-  /**
-   * Optional hero override.
-   * If not provided, the global defaultHero is used.
-   */
-  hero?: HeroDefinition;
-};
-
-export function UniversalHero({ hero = defaultHero }: UniversalHeroProps) {
-  return <HeroShell hero={hero} />;
+export function UniversalHero({ hero }: { hero?: HeroDefinition }) {
+  return <HeroShell hero={hero ?? defaultHero} />;
 }
